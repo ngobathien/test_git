8e21b29b7fa56cd92b84fa78a85b79a212383e95
ngobathien <bathienba1@gmail.com>
2025-07-01 21:35:14 +0700

    edit test.js, add 3

--- test.js
+++ test.js
@@ -10,3 +10,8 @@ function subtract(a, b) {
   return a - b;
 }
 console.log(subtract(10, 4)); // Outputs: 6
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(7, 6)); // Outputs: 42
