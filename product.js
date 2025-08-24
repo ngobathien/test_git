69c855b4bb004d88a3b9d2c1a489a5d5f5c25254
nbathien <thienngo533@gmail.com>
2025-08-24 13:34:43 +0700

    add updateProduct()

--- product.js
+++ product.js
@@ -7,3 +7,8 @@ function getProduct() {
 function addProduct(name) {
   console.log(`Thêm sản phẩm thành công ${name}`);
 }
+// dev B code cap nhat, xoa san pham
+function updateProduct(id, data) {
+  console.log("Updating product with ID:", id);
+  console.log("New data:", data);
+}
