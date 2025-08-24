02d228c60e66ef50962b7cb72653dc49961ac357
nbathien <thienngo533@gmail.com>
2025-08-24 13:42:59 +0700

    add deleteProduct(id)

--- product.js
+++ product.js
@@ -12,3 +12,7 @@ function updateProduct(id, data) {
   console.log("Updating product with ID:", id);
   console.log("New data:", data);
 }
+
+function deleteProduct(id) {
+  console.log("Deleting product with ID:", id);
+}
