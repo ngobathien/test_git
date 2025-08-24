327f119fe382948139f199ddd7b118185be5e1ea
ngobathien <bathienba1@gmail.com>
2025-08-24 13:43:48 +0700

    C2: add function addProduct()

--- product.js
+++ product.js
@@ -1,3 +1,9 @@
+// dev A code lấy, thêm sản phẩm
+
 function getProduct() {
   console.log(`Danh sách sản phẩm`);
+}
+
+function addProduct(name) {
+  console.log(`Thêm sản phẩm thành công ${name}`);
 }
